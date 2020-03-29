dfb6b9e424ae827a3061cdecc31e6d8d4f94fe15
nini chen <niniwiwichen@gmail.com>
2020-03-29 21:57:19 +0800

    Initial

---
+++ resources/views/index.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Skill Exchange</title>
+
+	<style>
+		
+		.loginHolder{
+			position: absolute;
+    		right: 50px;
+    		top: 80px;
+		}
+
+		.Button{
+			padding: 10px 20px;
+    		border-radius: 2px;
+    		margin-left: 10px;
+    		position: relative;
+    		display: inline-block;
+		    top: 0;
+		    left: 0;
+		    margin: 10px 10px;
+		    background-color: #FFFFC9;
+		    box-shadow: -2px 2px 2px 1px #F0F000;
+		    font-size: 100%;
+		    cursor: pointer;
+		    font-weight: bold;
+		}
+		.Button:active{
+			padding: 10px 20px;
+    		border-radius: 2px;
+    		margin-left: 10px;
+    		position: relative;
+    		display: inline-block;
+		    top: 0;
+		    left: 0;
+		    margin: 10px 10px;
+		    background-color: #FFFFC9;
+		    box-shadow: -2px 2px 2px 1px #FFFFF2;
+		    font-size: 100%;
+		    cursor: pointer;
+		}
+
+		.HeaderImage{
+			margin-left: 10px;
+    		margin-top: 20px;
+    		margin-block-end: 20px;
+    		width:480px;
+    		height:120px;
+    		cursor: pointer;
+		}
+
+		.SearchInput{
+			margin-top: 30px;
+			margin-left: 10px;
+			margin-right: 30px;
+    		padding-top: 5px;
+    		padding-bottom: 5px;
+    		padding-left: 10px;
+    		font-size: 90%;
+    		background-color: white;
+    		border-radius: 5px;
+    		border-style: solid;
+    		border-color: #D9D9D9;
+    		border-width: 1px;
+    		width: 20%;
+			box-sizing: border-box;
+			text-align: left;
+			float: right;
+		}
+
+
+	</style>
+
+</head>
+
+<body style="background-color:#FFFFB5;margin:0;font-family: 'Comic Sans MS','微軟正黑體'";>
+	<div id="header" style="background-color:#FFFFF2;">
+		
+
+		<img src="../static/title1.png" class="HeaderImage" onclick="location.href='{{route('home')}}';"/>
+
+    	<div class="loginHolder">
+
+    			<div class="Button" id ="login" onclick="location.href='{{route('login')}}';">登入</div>
+    			<div class="Button" id="Register" onclick="location.href='{{route('register')}}';">註冊</div>
+    			<div class="Button" id="Contact" onclick="location.href='{{route('contact')}}';">聯絡我們</div>
+    		
+    	</div>
+
+	</div>
+
+	<div>
+		<div id="Search">
+			<input type="text" class="SearchInput" placeholder="關鍵字搜尋" name="search">
+		</div>
+	</div>
+
+	<!-- <img src="../static/cuteCat.jpg" style="width:100%" /> -->
+</body>
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>SELogin</title>
+	<style>
+		@keyframes slideUp{
+			from{
+				opacity: 0;
+				top: 100%;
+			}
+			to{
+				opacity: 1;
+				top: 40%;
+			}
+		}
+		.LoginPage{
+			background-color:white;
+			max-width:400px;
+			position: fixed;
+		    left: 50%;
+		    transform: translate(-50%, -50%);
+		    border-radius: 10px;
+		    text-align: center;
+		    padding: 10px 20px 30px 20px;
+		    z-index: 100;
+		    animation: slideUp 1s ease 0s 1 normal both;
+		    width: 80%;
+		    font-size: 100%;
+/*		    box-shadow: -10px 10px 20px 1px #F0F000;*/
+		}
+
+		.Input{
+			margin-top: 5px;
+    		padding-top: 5px;
+    		padding-bottom: 5px;
+    		padding-left: 10px;
+    		font-size: 90%;
+    		background-color: white;
+    		border-radius: 5px;
+    		border-style: solid;
+    		border-color: #D9D9D9;
+    		border-width: 1px;
+    		width: 80%;
+			box-sizing: border-box;
+			text-align: left;
+		}
+
+		.option{
+			overflow: auto;
+    		margin-top: 15px;
+    		display:block;
+		}
+
+		.OptionButton{
+			padding: 10px 20px;
+    		border-radius: 2px;
+    		margin-left: 10px;
+    		position: relative;
+    		display: inline-block;
+		    top: 0;
+		    left: 0;
+		    margin: 5px 5px;
+		    background-color: #FFFFC9;
+		    box-shadow: -2px 2px 3px 1px #F0F000;
+		    font-size: 100%;
+		    cursor: pointer;
+		}
+
+		.OptionButton:active{
+			padding: 10px 20px;
+    		border-radius: 2px;
+    		margin-left: 10px;
+    		position: relative\;
+    		display: inline-block;
+		    top: 0;
+		    left: 0;
+		    margin: 5px 5px;
+		    background-color: #FFFFC9;
+		    box-shadow: -2px 2px 3px 1px white;
+		    font-size: 100%;
+		    cursor: pointer;
+		}
+
+
+	</style>
+
+</head>
+
+<body style="background-color:#FFFFB5;margin:0;font-family: '微軟正黑體'";>
+	
+	<div class = "LoginPage">
+		<img src = "../static/login.png";
+    	style = " display: block;
+    	max-width: 50%;
+    	margin: auto;
+    	margin-bottom: 10px;"/>
+    	<input type="text" class="Input" placeholder="電子信箱" name="email">
+    	<input type="password" class="Input" placeholder="密碼" name="password">
+
+    	<div class="option">
+    		<div class="OptionButton" id="sure">登入</div>
+    		<div class="OptionButton" id="forget">忘記密碼</div>
+    		<div class="OptionButton" id="register" onclick="location.href='{{route('register')}}';">註冊</div>
+    		<div class="OptionButton" id="close" onclick="location.href='{{route('home')}}';">關閉</div>
+    	</div>
+
+	</div>
+
+
+</body>
+</html>
